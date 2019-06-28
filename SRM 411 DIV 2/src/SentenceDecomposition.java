e6cda6dcc7401237db5a4da54e072678aa9d31b6
ziggy192 <luuquangnghia97@gmail.com>
2019-06-28 10:15:57 +0700

    Refine 411 Div 2 Prob (Quy hoach dong with <3)

--- SRM 411 DIV 2/src/SentenceDecomposition.java
+++ SRM 411 DIV 2/src/SentenceDecomposition.java
@@ -1,109 +1,87 @@
 import java.util.*;
 import java.math.*;
+
 import static java.lang.Math.*;
 
 public class SentenceDecomposition {
 
-	private List<String>[] itegrationFormWords;
-	public int decompose(String sentence, String[] validWords) {
-		int maxLength = 0;
-		for (String validWord : validWords) {
-			if (validWord.length() > maxLength) {
-				maxLength = validWord.length();
-			}
-		}
+    private List<String>[] itegrationFormWords;
 
-		int[][] l = new int[validWords.length][sentence.length()];
-		for (int[] ints : l) {
+    public int decompose(String sentence, String[] validWords) {
+        int[] l = new int[sentence.length()];
+        Arrays.fill(l, -1);
+        for (int i = 0; i < sentence.length(); i++) {
 
-			Arrays.fill(ints,-1);
-		}
+            // TODO: 6/28/19 find min
+            int min = Integer.MAX_VALUE;
+            for (String validWord : validWords) {
+                if (i - validWord.length() + 1 < 0) {
+                    continue;
+                }
+                String checkingWord = sentence.substring(i - validWord.length() + 1, i + 1);
+                int cost = getCost(checkingWord, validWord);
+                if (cost == -1) {
+                    continue;
+                }
+                int length;
+                if (i - validWord.length() < 0) {
+                    length = cost;
+                } else if (l[i - validWord.length()] != -1) {
 
-		for (int i = 0; i < sentence.length(); i++) {
-			for (int j = 0; j < validWords.length; j++) {
-				if (i - validWords[j].length()+1 < 0) {
-					continue;
-				}
-				int cost = getCost(sentence.substring(i - validWords[j].length() + 1, i + 1), validWords[j]);
-				if (cost == -1) {
-					l[j][i] = -1;
-					continue;
-				}
-				int min = Integer.MAX_VALUE;
-				// TODO: 6/28/19 find min
-				if (i - validWords[j].length() < 0) {
-					min = 0;
-
-				} else {
-
-					for (int k = 0; k < validWords.length; k++) {
-						if (i - validWords[j].length() < 0) {
-							continue;
-						}
-						if ((l[k][i - validWords[j].length()] < min) && l[k][i - validWords[j].length()] != -1) {
-							min = l[k][i - validWords[j].length()];
-						}
-
-					}
-				}
-
-				if (min != Integer.MAX_VALUE) {
-
-					l[j][i] = cost + min;
-				} else {
-					l[j][i] = -1;
-				}
-			}
-
-		}
-		int min = Integer.MAX_VALUE;
-		// TODO: 6/28/19 find min
-		for (int k = 0; k <validWords.length; k++) {
-			if (l[k][sentence.length()-1] < min && l[k][sentence.length()-1]!=-1) {
-				min = l[k][sentence.length()-1];
-			}
-
-		}
-		if (min == Integer.MAX_VALUE) {
-			min = -1;
-		}
-		return min;
-	}
+                    length = cost + l[i - validWord.length()];
+                } else {
+                    length = -1;
+                }
 
 
-	private int getCost(String checkingWord, String validWord) {
-		if (checkingWord.length() != validWord.length()) {
-			return -1;
-		}
-
-		boolean[] flag = new boolean[validWord.length()];
-
-		char[] checkingWordChars = checkingWord.toCharArray();
-		char[] validWordChars = validWord.toCharArray();
-		for (char checkingWordChar : checkingWordChars) {
-			boolean foundTheChar = false;
-			for (int i = 0; i < validWordChars.length; i++) {
-				if (validWordChars[i] == checkingWordChar && flag[i] == false) {
-					flag[i] = true;
-					foundTheChar = true;
-					break;
-				}
-			}
-			if (!foundTheChar) {
-				return -1;
-			}
+                if (length != -1 && length < min) {
+                    min = length;
+                }
 
 
-		}
+            }
+            if (min == Integer.MAX_VALUE) {
+                min = -1;
+            }
+            l[i] = min;
+        }
 
-		int count = 0;
-		for (int i = 0; i < checkingWordChars.length; i++) {
-			if (checkingWordChars[i] != validWordChars[i]) {
-				count++;
-			}
-		}
-		return count;
-	}
+        return l[sentence.length() - 1];
+    }
+
+    public int getCost(String checkingWord, String validWord) {
+        if (checkingWord.length() != validWord.length()) {
+            return -1;
+        }
+        // TODO: 6/28/19 check enough characters
+
+        boolean[] flag = new boolean[checkingWord.length()];
+        char[] checkingWordChars = checkingWord.toCharArray();
+        char[] validWordChars = validWord.toCharArray();
+        for (char checkingWordChar : checkingWordChars) {
+            boolean foundChar = false;
+            for (int i = 0; i < validWordChars.length; i++) {
+                char validWordChar = validWordChars[i];
+                if (validWordChar == checkingWordChar && flag[i] == false) {
+                    flag[i] = true;
+                    foundChar = true;
+                    break;
+                }
+
+            }
+            if (!foundChar) {
+                return -1;
+            }
+        }
+        int count = 0;
+        for (int i = 0; i < checkingWordChars.length; i++) {
+            if (checkingWordChars[i] != validWordChars[i]) {
+                count++;
+            }
+        }
+        // TODO: 6/28/19 cacluate cost
+        return count;
+    }
 
 
 }
